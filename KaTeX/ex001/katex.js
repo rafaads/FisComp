6088dd4583658e1814b5fc75a8cf3aac48235a7b
rafaaelds <rafaaelds@gmail.com>
2019-12-30 17:04:09 -0300

    Intro ao KaTeX
    Intro ao KaTex

---
+++ KaTeX/ex001/katex.js
@@ -0,0 +1,13 @@
+// npm install katex
+// npm install -g katex
+
+import katex from 'katex/dist/katex.mjs'
+
+//euler integral 
+
+
+const katex = require('katex');
+
+\Gamma(n) = (n-1)!\quad\forall n\in\
+
+\Gamma(z) = \int_0^\infty t^{z-1}e^{-t}dt\
